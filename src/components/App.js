8bd911515fd5098010cfddcce738b34d180efb38
bwrosen20 <bwrosen20@gmail.com>
2023-02-21 06:27:22 -0500

    Final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useReducer } from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+
+//renders all properties
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} color={user.color} city={user.city}/>
+      <About bio={user.bio} linkedin={user.links.linkedin} github={user.links.github}/>
+
     </div>
   );
 }
 
 export default App;
+
+
